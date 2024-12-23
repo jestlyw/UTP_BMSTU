534071f7c9e686354427b576a6f456a8a7ed63c2
jestlyw <jestlyw@gmail.com>
2024-12-23 18:42:28 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
-public class App {
+republic class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello!");
+        System.out.println("Hello! everybody!");
     }
 }
